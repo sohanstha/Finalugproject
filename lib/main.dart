e2125a0401f237a81b8ca7ba4d0ec283c03d87bd
sohanstha <Sohanstha12@gmail.com>
2023-04-16 11:30:16 +0545

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// @dart=2.9
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitness_gymapp/firebase_options.dart';
+import 'package:fitness_gymapp/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'router.dart' as router;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+    onGenerateRoute: router.generateRoute,
+  ));
+}
